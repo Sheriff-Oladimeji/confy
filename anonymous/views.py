90d0eb276d69d0a1ad81c6334cae898985945e25
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-01-03 21:43:31 +0100

    feat: add url configs

--- anonymous/views.py
+++ anonymous/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Note
 # Create your views here.
+
+
+def home(request):
+    notes  = Note.objects.all().values()
+    context = {"notes": notes}
+    return render(request, 'home.html', context)
